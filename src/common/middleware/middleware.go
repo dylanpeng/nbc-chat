00488dfeaadaf5db85ea9d71d6a78a575700784a
peng.chen <chen_peng7@163.com>
2023-05-01 19:40:20 +0800

    20230501 update middleware

--- src/common/middleware/middleware.go
+++ src/common/middleware/middleware.go
@@ -25,9 +25,12 @@ func CheckEncoding(ctx *gin.Context) {
 
 func CrossDomain(ctx *gin.Context) {
 	ctx.Header("Access-Control-Allow-Origin", "*")
-	ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-	ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, UserId")
-	ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+	//ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	//ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, UserId")
+	//ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+	ctx.Header("Access-Control-Allow-Methods", "*")
+	ctx.Header("Access-Control-Allow-Headers", "*")
+	ctx.Header("Access-Control-Expose-Headers", "*")
 	ctx.Header("Access-Control-Allow-Credentials", "true")
 
 	if ctx.Request.Method == "OPTIONS" {
